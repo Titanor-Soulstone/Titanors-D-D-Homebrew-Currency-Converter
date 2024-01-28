9db72847f6af8b08db83063be6ef2b101bd1c6ac
Titanor Soulstone <sir.titas@yahoo.com.au>
2024-01-28 19:36:17 +0800

    Update darkmode.js - fixed a bug

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,42 @@
-// ... (existing dark mode code)
+// Dark Mode Toggle Function
+function toggleDarkMode() {
+  const body = document.body;
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const sunMoon = document.getElementById('sunMoon');
+
+  if (darkModeToggle.checked) {
+    body.classList.add('dark-mode');
+    sunMoon.innerHTML = '&#x1F319;'; // Moon symbol
+    setCookie('darkMode', 'enabled', 365);
+  } else {
+    body.classList.remove('dark-mode');
+    sunMoon.innerHTML = '&#x2600;'; // Sun symbol
+    setCookie('darkMode', 'disabled', 365);
+  }
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  const cookieName = name + '=';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
 
 // Function to apply dark mode styles to iframe content
 function applyDarkModeToIframe() {
