70a3b9386bf0e833b777f623795850e7f71f1e25
Titanor Soulstone <sir.titas@yahoo.com.au>
2024-01-28 22:41:46 +0800

    fixed a bug

--- darkmode.js
+++ darkmode.js
@@ -28,6 +28,53 @@ function applyDarkModeToIframe() {
   });
 }
 
+// Function to set a cookie
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  const cookieName = name + '=';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
+
+// Function to accept cookies
+function acceptCookies() {
+  document.getElementById('cookieConsent').style.display = 'none';
+  setCookie('cookieConsent', 'accepted', 365);
+
+  // Check and apply dark mode preference
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const sunMoon = document.getElementById('sunMoon');
+  const darkModeCookie = getCookie('darkMode');
+
+  if (darkModeCookie === 'enabled') {
+    darkModeToggle.checked = true;
+    document.body.classList.add('dark-mode');
+    sunMoon.innerHTML = '&#x1F319;'; // Moon symbol
+  } else {
+    darkModeToggle.checked = false;
+    document.body.classList.remove('dark-mode');
+    sunMoon.innerHTML = '&#x2600;'; // Sun symbol
+  }
+
+  // Apply dark mode styles to the iframe content
+  applyDarkModeToIframe();
+}
+
 // Event listener for dark mode toggle
 document.getElementById('darkModeToggle').addEventListener('change', function() {
   toggleDarkMode();
