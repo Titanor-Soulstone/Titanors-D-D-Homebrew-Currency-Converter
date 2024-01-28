beef2b6fb0fe4b27512c8e9e011d505b64a33505
Titanor Soulstone <sir.titas@yahoo.com.au>
2024-01-28 20:08:49 +0800

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,19 @@
+// Function to get a cookie
+function getCookie(name) {
+  const cookieName = name + '=';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
+
 // Dark Mode Toggle Function
 function toggleDarkMode() {
   const body = document.body;
