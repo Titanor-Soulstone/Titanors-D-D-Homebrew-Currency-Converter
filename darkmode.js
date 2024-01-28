37bccc883e1d4ab63d292ce1d6f1346b56047662
Titanor Soulstone <sir.titas@yahoo.com.au>
2024-01-28 19:17:45 +0800

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,77 @@
+// Dark Mode Toggle Function
+function toggleDarkMode() {
+  const body = document.body;
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const sunMoon = document.getElementById('sunMoon');
+
+  if (darkModeToggle.checked) {
+    body.classList.add('dark-mode');
+    sunMoon.innerHTML = '&#x1F319;'; // Moon symbol
+    setCookie('darkMode', 'enabled', 365);
+  } else {
+    body.classList.remove('dark-mode');
+    sunMoon.innerHTML = '&#x2600;'; // Sun symbol
+    setCookie('darkMode', 'disabled', 365);
+  }
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = name + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  const cookieName = name + '=';
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
+
+// Function to accept cookies
+function acceptCookies() {
+  document.getElementById('cookieConsent').style.display = 'none';
+  setCookie('cookieConsent', 'accepted', 365);
+
+  // Check and apply dark mode preference
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const sunMoon = document.getElementById('sunMoon');
+  const darkModeCookie = getCookie('darkMode');
+
+  if (darkModeCookie === 'enabled') {
+    darkModeToggle.checked = true;
+    document.body.classList.add('dark-mode');
+    sunMoon.innerHTML = '&#x1F319;'; // Moon symbol
+  } else {
+    darkModeToggle.checked = false;
+    document.body.classList.remove('dark-mode');
+    sunMoon.innerHTML = '&#x2600;'; // Sun symbol
+  }
+}
+
+// Check and apply dark mode preference on initial load
+window.addEventListener('load', function () {
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const sunMoon = document.getElementById('sunMoon');
+  const darkModeCookie = getCookie('darkMode');
+
+  if (darkModeCookie === 'enabled') {
+    darkModeToggle.checked = true;
+    document.body.classList.add('dark-mode');
+    sunMoon.innerHTML = '&#x1F319;'; // Moon symbol
+  } else {
+    darkModeToggle.checked = false;
+    document.body.classList.remove('dark-mode');
+    sunMoon.innerHTML = '&#x2600;'; // Sun symbol
+  }
+});
